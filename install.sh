29b979d1eccd2516ce942bc05cde60cb4d319b59
Pauloledes <40398982+Pauloledes@users.noreply.github.com>
2021-04-23 09:41:37 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Installation des librairies Python
+pip3 install opencv-python
+pip3 install os
+pip3 install shutil
+pip3 install pandas
+pip3 install glob
+pip3 install subprocess
+pip3 install time
+
+
+#Installation de ffmpeg
+sudo apt install ffmpeg
+
+#Installation d'OpenFace
+git clone https://github.com/TadasBaltrusaitis/OpenFace.git
+
